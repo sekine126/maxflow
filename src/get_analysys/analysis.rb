9948cfeecf3b61bad04d0cf45f34cb0dd44d7f99
YutakaSekine <llik.iris@gmail.com>
2017-01-08 09:58:06 +0900

    Add Analysis class

---
+++ src/get_analysys/analysis.rb
@@ -0,0 +1,158 @@
+# 2つのWebコミュニティファイルを解析して
+# 再現率、適合率、F値を求めるクラス
+
+class Analysis
+
+  # 初期化メソッド
+  # file: 初期コミュニティファイルパス
+  # file1: X日後のフルクロールコミュニティファイルパス
+  # file2: X日後の局所クロールコミュニティファイルパス
+  def initialize(file,file1,file2)
+    @com_filepath = file
+    @com1_filepath = file1
+    @com2_filepath = file2
+    @hashs = get_hashs(@com_filepath)
+    @hashs1 = get_hashs(@com1_filepath)
+    @hashs2 = get_hashs(@com2_filepath)
+    @add_hashs1 = get_add_hashs1(@hashs,@hashs1)
+    @add_hashs2 = get_add_hashs2(@hashs,@hashs2)
+    @drop_hashs1 = get_drop_hashs1(@hashs,@hashs1)
+    @drop_hashs2 = get_drop_hashs2(@hashs,@hashs2)
+    @cha_hashs1 = get_cha_hashs1(@hashs,@hashs1)
+    @cha_hashs2 = get_cha_hashs2(@hashs,@hashs2)
+  end
+
+  # コミュニティ再現率を取得
+  def get_community_recall
+    recall = get_recall(@hashs1, @hashs2)
+  end
+
+  # 増加分再現率を取得
+  def get_addition_recall
+    recall = get_recall(@add_hashs1, @add_hashs2)
+  end
+
+  # 減少分再現率を取得
+  def get_drop_recall
+    recall = get_recall(@drop_hashs1, @drop_hashs2)
+  end
+
+  # 変化分再現率を取得
+  def get_change_recall
+    recall = get_recall(@cha_hashs1, @cha_hashs2)
+  end
+
+  # コミュニティ適合率を取得
+  def get_community_precision
+    precision = get_precision(@hashs1, @hashs2)
+  end
+
+  # 増加分適合率を取得
+  def get_addition_precision
+    precision = get_precision(@add_hashs1, @add_hashs2)
+  end
+
+  # 減少分適合率を取得
+  def get_drop_precision
+    precision = get_precision(@drop_hashs1, @drop_hashs2)
+  end
+
+  # 変化分適合率を取得
+  def get_change_precision
+    precision = get_precision(@cha_hashs1, @cha_hashs2)
+  end
+
+  # F値を取得
+  # r: 再現率
+  # p: 適合率
+  def get_f(r,p)
+    f = ((r*p*2)/(r+p)).round(2)
+  end
+
+
+  private
+
+  # コミュニティデータを取得するメソッド
+  # filepath: コミュニティファイルパス
+  def get_hashs(filepath)
+    hashs = Hash.new()
+    open(filepath) {|file|
+      while l = file.gets
+        hashs[l.split(",")[0].to_i] = 1
+        hashs[l.split(",")[1].to_i] = 1
+      end
+    }
+    hashs
+  end
+
+  # 再現率を取得するメソッド
+  # hashs1: 正解クラスタデータ
+  # hashs2: 対象クラスタデータ
+  def get_recall(hashs1, hashs2)
+    r_nodes = []
+    hashs1.each do |key1, value1|
+      if nodes2_hash[key1] == 1
+        r_nodes << key1
+      end
+    end
+    recall = (r_nodes.size/hashs1.size.to_f*100).round(2)
+  end
+
+  # 適合率を取得するメソッド
+  # hashs1: 正解クラスタデータ
+  # hashs2: 対象クラスタデータ
+  def get_precision(hashs1, hashs2)
+    p_nodes = []
+    hashs1.each do |key1, value1|
+      if nodes2_hash[key1] == 1
+        p_nodes << key1
+      end
+    end
+    recall = (p_nodes.size/hashs2.size.to_f*100).round(2)
+  end
+
+  # 初期コミュニティから追加されたページを取得するメソッド
+  # hashs1: 初期コミュニティデータ
+  # hashs2: X日後コミュニティデータ
+  def get_add_hashs(hashs1,hashs2)
+    hashs = []
+    hashs2.each do |key2, value2|
+      if hashs1[key2] == nil
+        hashs << key2
+      end
+    end
+    hashs
+  end
+
+  # 初期コミュニティから除かれたページを取得するメソッド
+  # hashs1: 初期コミュニティデータ
+  # hashs2: X日後コミュニティデータ
+  def get_drop_hashs(hashs1,hashs2)
+    hashs = []
+    hashs1.each do |key1, value1|
+      if hashs2[key1] == nil
+        hashs << key1
+      end
+    end
+    hashs
+  end
+
+  # 初期コミュニティから変化したページを取得するメソッド
+  # hashs1: 初期コミュニティデータ
+  # hashs2: X日後コミュニティデータ
+  def get_cha_hashs(hashs1,hashs2)
+    hashs = []
+    hashs1.each do |key1, value1|
+      if hashs2[key1] == nil
+        hashs << key1
+      end
+    end
+    hashs2.each do |key2, value2|
+      if hashs1[key2] == nil
+        hashs << key2
+      end
+    end
+    hashs
+  end
+
+end
---
+++ src/get_analysys/get_betweenness_analysis.rb
@@ -0,0 +1,89 @@
+# 2つのWebコミュニティファイルから再現率、適合率、F値を求めるプログラム
+# bundle exec ruby src/get_analysis/get_random_analysys.rb -d sample -f 20161220 -t 20161221
+# -d データベースネーム
+# -f 日付1
+# -t 日付2
+require "./src/get_analysys/analysis.rb"
+
+params = ARGV.getopts('d:f:t:')
+if params["d"] == nil
+  puts "Error: Please set -d database name."
+  exit(1)
+end
+if params["f"] == nil
+  puts "Error: Please set -f date option."
+  exit(1)
+end
+if params["f"] != nil && params["f"].size != 8 
+  puts "Error: -f is date. e.g. 20150214"
+  exit(1)
+end
+if params["t"] == nil
+  puts "Error: Please set -t date option."
+  exit(1)
+end
+if params["t"] != nil && params["f"].size != 8 
+  puts "Error: -t is date. e.g. 20150214"
+  exit(1)
+end
+
+# コマンドライン引数を取得
+db_name = params["d"]
+date1 = params["f"]
+date2 = params["t"]
+
+# ファイルパスを作成
+filepath = "./data/crawl/#{db_name}_crawl_#{date1}.txt"
+filepath1 = "./data/crawl/#{db_name}_crawl_#{date2}.txt"
+filepath2 = "./data/update/bet_#{db_name}_#{date1}_#{date2}.txt"
+
+# 解析クラスのインスタンスメソッドを作成
+analysis = Analysis.new(filepath, filepath1, filepath2)
+
+# コミュニティ再現率を表示
+community_recall = analysis.get_community_recall()
+puts "Community Recall #{community_recall} %"
+
+# コミュニティ適合率を表示
+community_precision = analysis.get_community_precision()
+puts "Community Precision #{community_precision} %"
+
+# コミュニティF値を表示
+community_f = analysis.get_f(community_recall, community_precision)
+puts "Community F #{community_f}"
+
+# 増加分再現率を表示
+addition_recall = analysis.get_addition_recall
+puts "Addition Recall #{addition_recall} %"
+
+# 増加分適合率を表示
+addition_precision = analysis.get_addition_precision
+puts "Addition Precision #{addition_precision} %"
+
+# 増加分F値を表示
+addition_f = analysis.get_f(addition_recall, addition_precision)
+puts "Addition F #{addition_f}"
+
+# 減少分再現率を表示
+drop_recall = analysis.get_drop_recall
+puts "Drop Recall #{drop_recall} %"
+
+# 減少分適合率を表示
+drop_precision = analysis.get_drop_precision
+puts "Drop Precision #{drop_precision} %"
+
+# 減少分F値を表示
+drop_f = analysis.get_f(drop_recall, drop_precision)
+puts "Drop F #{drop_f}"
+
+# 変化分再現率を表示
+change_recall = analysis.get_change_recall
+puts "Change Recall #{change_recall} %"
+
+# 変化分適合率を表示
+change_precision = analysis.get_change_precision
+puts "Change Precision #{change_precision} %"
+
+# 変化分F値を表示
+change_f = analysis.get_f(change_recall, change_precision)
+puts "Change F #{change_f}"
